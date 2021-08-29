5665403fc6ca3d53c93885623277ffcea815af66
bilaltahir2178 <bilaltahir2178@gmail.com>
2021-08-29 13:51:43 +0500

    Added Task model.

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Task:
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        is_completed = 'Completed' if self.completed else 'Incomplete'
+        return self.title + " -> " + is_completed
+        
